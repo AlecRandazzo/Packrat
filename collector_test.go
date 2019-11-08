e10b31124913548ad85c473a4b35533f314d4acb
Alec Randazzo <alec.randazzo@gmail.com>
2019-11-07 22:48:25 -0500

    Dev (#14)
    * unit tests

---
+++ collector_test.go
@@ -0,0 +1,46 @@
+package windowscollector
+
+import "testing"
+
+func TestCollect(t *testing.T) {
+	type args struct {
+		exportList   ListOfFilesToExport
+		resultWriter ResultWriter
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Collect(tt.args.exportList, tt.args.resultWriter); (err != nil) != tt.wantErr {
+				t.Errorf("Collect() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_getFiles(t *testing.T) {
+	type args struct {
+		volumeHandler        *VolumeHandler
+		resultWriter         ResultWriter
+		listOfSearchKeywords listOfSearchTerms
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := getFiles(tt.args.volumeHandler, tt.args.resultWriter, tt.args.listOfSearchKeywords); (err != nil) != tt.wantErr {
+				t.Errorf("getFiles() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
